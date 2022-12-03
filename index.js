f17552a4efb671a4bce642f2662ccc17195b9c30
DominykMcNamara <dominyksmith@gmail.com>
2022-12-02 19:44:28 -0500

    Commented & refractored

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 const messages = require("./data.json");
 
-const randomIndexGenerator = () => {
-  return Math.floor(Math.random() * messages.length);
+const generateRandomMessage = (data = messages) => {
+  // Generate a random index
+  let randomIndex = Math.floor(Math.random() * data.length)
+  let generatedMessage = [];
+  // If the data only has 1 element we instantly return that element
+  if (data.length === 1) {
+    generatedMessage = data.toString();
+  }
+  // Determine a random index
+  let retrievedMessage = data[randomIndex];
+  // Push elements of data into the generatedMessage array
+  Object.values(retrievedMessage).forEach((values) => {
+    generatedMessage.push(values);
+  });
+  // Log & return
+  console.log(generatedMessage.join(""));
+  return generatedMessage
 };
 
-const generateRandomMessage = (data = messages) => {
-  let generatedMessage = [];
-  let randomIndex = randomIndexGenerator();
-  if (data.length === 1) {
-    return generatedMessage.toString();
-  }
-  let retrievedMessage = data[randomIndex];
-  Object.values(retrievedMessage).forEach((values) => {
-    
-  });
-};
-generateRandomMessage(messages);
+module.exports = { generateRandomMessage }
